9041ca992b2e33c358e2f585678e85e5fe13dcc6
Thomas von Deyen <vondeyen@blish.cloud>
2025-11-05 17:45:47 +0100

    wip: fix seeding promotion sets
    we need to require them
    
    also added promotion category to edit role form

--- core/db/default/spree/permission_sets.rb
+++ core/db/default/spree/permission_sets.rb
@@ -1,5 +1,27 @@
 # frozen_string_literal: true
 
+files = []
+
+Spree::Core::Engine.root.join('app', 'models', 'spree', 'permission_sets', '**', '*.rb').tap do |path|
+  files += Dir.glob(path.to_s)
+end
+
+if defined?(SolidusLegacyPromotions)
+  SolidusLegacyPromotions::Engine.root.join('app', 'models', 'spree', 'permission_sets', '**', '*.rb').tap do |path|
+    files += Dir.glob(path.to_s)
+  end
+end
+
+if defined?(SolidusPromotions)
+  SolidusPromotions::Engine.root.join('app', 'models', 'solidus_promotions', 'permission_sets', '**', '*.rb').tap do |path|
+    files += Dir.glob(path.to_s)
+  end
+end
+
+files.each do |file|
+  require file
+end
+
 Spree::PermissionSets::Base.subclasses.each do |permission|
   Spree::PermissionSet.create!(
     name: permission.name.demodulize,
